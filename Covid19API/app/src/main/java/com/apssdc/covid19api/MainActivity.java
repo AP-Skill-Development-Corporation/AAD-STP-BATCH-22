99584aa34615e1dd2cc704530d0c94351f56873b
sivaprasad <siva.bommini@gmail.com>
2020-10-19 19:14:52 +0530

    rectrofit added

--- Covid19API/app/src/main/java/com/apssdc/covid19api/MainActivity.java
+++ Covid19API/app/src/main/java/com/apssdc/covid19api/MainActivity.java
@@ -3,7 +3,14 @@ package com.apssdc.covid19api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,11 +19,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView text;
+ProgressBar pg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text_view);
+        pg=findViewById(R.id.prog);
        Retrofit retrofit=new Retrofit.Builder().baseUrl("https://api.covid19api.com/")
                .addConverterFactory(ScalarsConverterFactory.create()).build();
        Covid19Interface service=retrofit.create(Covid19Interface.class);
@@ -24,7 +34,22 @@ public class MainActivity extends AppCompatActivity {
         data.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                Toast.makeText(MainActivity.this,response.body() , Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this,response.body() , Toast.LENGTH_SHORT).show();
+                try {
+                    JSONArray root=new JSONArray(response.body());
+                    for (int i=0;i<root.length();i++){
+                        JSONObject data=root.getJSONObject(i);
+                        String date=data.getString("Date");
+                        int confirmed=data.getInt("Confirmed");
+                        int deathes=data.getInt("Deaths");
+                        int active=data.getInt("Active");
+                        text.append(confirmed +"-"+active+"-"+"-"+deathes+"\n");
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    pg.setVisibility(View.GONE);
+                }
 
             }
 
